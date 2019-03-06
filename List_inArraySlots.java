f07fb56a5678a44ffcd43cbaac03e23f2329152c
lrhasher <lrhasher@gmail.com>
2019-03-06 15:51:31 -0500

    updated List_inArraySlots

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -6,11 +6,15 @@
 public class List_inArraySlots {
 
     // declare fields here
+    private int[] list; //the array
+    private int slotsFilled; //num of slot in array filled
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
+        list = new int[5];
+        slotsFilled = 0;
     }
 
 
@@ -18,6 +22,7 @@ public class List_inArraySlots {
       @return the number of elements in this list
      */
     public int size() {
+        return slotsFilled;
     }
 
 
@@ -26,6 +31,11 @@ public class List_inArraySlots {
        in [a,b,c,] format
       */
     public String toString() {
+        String output = "[";
+        for (int i = 0; i < slotsFilled; i++) {
+            output += list[i] + ",";
+        }
+        return output + "]";
     }
 
 
@@ -34,7 +44,11 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-     public boolean add( int value) {
+    public boolean add( int value) {
+        if(slotsFilled = list.length)
+            expand();
+        list[slotsFilled++] = value;
+        return true;
      }
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# List_inArraySlots: An Introduction to ArrayLists
+# implement a list by storing data in an encapsulated array
 ## Partners: @AdamCohen0, @lrhasher, @OscarDorr
 ###### Constructor:
     public List_inArraySlots() {
@@ -10,7 +10,6 @@ Methods, fields are still unclear based on information given.
 Remarks: UserOfList should be capable of utilizing methods in List_inArraySlots to
 imitate or effectively represent lists in Java.
 
-# implement a list by storing data in an encapsulated array
 List_inArraySlots stores integers with any value that datatype can hold.
 
 That implies that the implementation cannot identify the end of the list by the values of the integers stored in the list. That design decision would require reserving one integer value as a value that cannot be stored in the list.
